6b92d2055e4eabda0192ae64bc38bd46fa44e974
Deejs2 <jireldhiraj123@gmail.com>
2025-02-11 20:51:45 +0545

    inital setup for the project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent) },
+    { path: 'game', loadComponent: () => import('./pages/game/game.component').then(m => m.GameComponent) },
+    { path: 'admin', loadComponent: () => import('./pages/admin/admin.component').then(m => m.AdminComponent) },
+    { path: '**', redirectTo: '' }
+  ];
